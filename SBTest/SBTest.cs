226e14e5709e443063b048871581dd249454afb4
Stephen Hung <hung.stephen.yh@gmail.com>
2017-08-01 08:05:39 +0800

    Added SendMessage Functionality

--- SBTest/SBTest.cs
+++ SBTest/SBTest.cs
@@ -34,19 +34,14 @@ namespace SBTest
                 Text = "Enter text here!"
             };
 
-            var sendTextButton = new Button
+            testingButton.Clicked += (object sender, EventArgs e) =>
             {
-                Text = "Send Text"
-            };
-
-            testingButton.Clicked += (object sender, EventArgs e) => {
                 System.Diagnostics.Debug.WriteLine("Heres some text to prove that the button works");
             };
 
             initSendbird.Clicked += initButtonHandler;
             connectSendbird.Clicked += connectButtonHandler;
             enterChannelButton.Clicked += joinChannelHandler;
-            sendTextButton.Clicked += sendTextHandler;
 
             ContentPage page = new ContentPage();
             StackLayout view = new StackLayout
@@ -60,7 +55,6 @@ namespace SBTest
             view.Children.Add(connectSendbird);
             view.Children.Add(enterChannelButton);
             view.Children.Add(textBox);
-            view.Children.Add(sendTextButton);
 
             MainPage = page;
         }
@@ -71,37 +65,46 @@ namespace SBTest
             System.Diagnostics.Debug.WriteLine("Success in initializing SendBird!");
         }
 
-        public void connectButtonHandler(object sender, EventArgs e){
+        public void connectButtonHandler(object sender, EventArgs e)
+        {
             string userId = "admin";
             string accessToken = "92089f76d32b534685fc9dcf78bfc2bdc6e3b2b9";
             SendBird.SendBirdClient.Connect(userId, accessToken, (User user, SendBirdException sendException) =>
             {
-                if(sendException != null){
+                if (sendException != null)
+                {
                     // An error has occurred while connecting.
                     System.Diagnostics.Debug.WriteLine("An error has occurred " +
                                                        "in connectButtonHandler while connecting");
                     System.Diagnostics.Debug.WriteLine("error: " + sendException.Code);
-                }else{
+                }
+                else
+                {
                     // Success!
                     System.Diagnostics.Debug.WriteLine("Success in connecting to SendBird!");
                 }
-               
+
             });
         }
 
-        public void joinChannelHandler(object sender, EventArgs e){
-            OpenChannel.GetChannel("testing", (channel, sendExcep) => {
+        public void joinChannelHandler(object sender, EventArgs e)
+        {
+            OpenChannel.GetChannel("testing", (channel, sendExcep) =>
+            {
                 if (sendExcep != null)
                 {
                     // An error has occurred while connecting.
                     System.Diagnostics.Debug.WriteLine("An error has occurred " +
                                                        "in joinChannelHandler while joining");
                     System.Diagnostics.Debug.WriteLine("error: " + sendExcep.Code);
-                }else{
+                }
+                else
+                {
                     System.Diagnostics.Debug.WriteLine("Success in joining a channel in SendBird!");
                 }
 
-                channel.Enter((SendBirdException enterExcep) => {
+                channel.Enter((SendBirdException enterExcep) =>
+                {
                     if (enterExcep != null)
                     {
                         System.Diagnostics.Debug.WriteLine("An error has occurred " +
@@ -109,50 +112,22 @@ namespace SBTest
                         System.Diagnostics.Debug.WriteLine("error: " + sendExcep.Code);
                     }
                 });
-            });
-        }
 
-        public void sendTextHandler(object sender, EventArgs eventE){
-            string currentText = textBox.Text;
-            System.Diagnostics.Debug.WriteLine("Current text in text box is: {0}", currentText);
-            //channel.SendUserMessage(currentText, (UserMessage message, SendBirdException e) => {
-            //    if (e != null)
-            //    {
-            //        // An error has occurred while sending a message.
-            //        System.Diagnostics.Debug.WriteLine("An error has occurred " +
-            //                                           "in sendTextHandler while sending a message");
-            //        System.Diagnostics.Debug.WriteLine("error: " + e.Code);
-                    
-            //    }else{
-            //        System.Diagnostics.Debug.WriteLine("Successfull sent message: {0}",currentText);
-            //    } 
-
-            //});
-            try
-            {
-                channel.SendUserMessage(currentText, (message, e) =>
+                channel.SendUserMessage("Test", (message, sendUserTestE) =>
                 {
-                    if (e != null)
+                    if (sendUserTestE != null)
                     {
-                        System.Diagnostics.Debug.WriteLine("currentText: {0}",currentText);
-
-                        System.Diagnostics.Debug.WriteLine("error when sending message");
-                        return;
+                        System.Diagnostics.Debug.WriteLine("Error occurred when sending user message" +
+                                                          " from inside joinChannelHandler");
+                        System.Diagnostics.Debug.WriteLine("error: " + sendUserTestE.Code);
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("Successfully sent message");
                     }
                 });
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.StackTrace);
-                System.Diagnostics.Debug.WriteLine(currentText);
-                System.Diagnostics.Debug.WriteLine(sender);
-                System.Diagnostics.Debug.WriteLine(eventE);
-                System.Diagnostics.Debug.WriteLine(channel);
-                return;
-            }
-
+            });
         }
-
 
 
         protected override void OnStart()
